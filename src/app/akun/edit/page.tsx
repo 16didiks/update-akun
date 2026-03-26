b9b065b85f4705f523395b9f00509014afcc141d
16didiks <16didiks@gmail.com>
2026-03-26 15:00:18 +0700

    fix: dynamic page for useSearchParams

--- src/app/akun/edit/page.tsx
+++ src/app/akun/edit/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Suspense } from 'react'
 import EditAkunForm from '@/features/akun/form/EditAkunForm'
+export const dynamic = 'force-dynamic'
 
 export default function Page() {
   return (
