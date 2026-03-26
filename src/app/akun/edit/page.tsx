678ea9114e0a5ca6532a3b0d99e14b450dab4823
16didiks <16didiks@gmail.com>
2026-03-26 15:17:26 +0700

    Update page.tsx

--- src/app/akun/edit/page.tsx
+++ src/app/akun/edit/page.tsx
@@ -1,11 +1,13 @@
 'use client'
+
 import { Suspense } from 'react'
 import EditAkunForm from '@/features/akun/form/EditAkunForm'
+
 export const dynamic = 'force-dynamic'
 
 export default function Page() {
   return (
-    <Suspense>
+    <Suspense fallback={<div>Loading...</div>}>
       <EditAkunForm />
     </Suspense>
   )
